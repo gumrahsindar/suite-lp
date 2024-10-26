b02ac15db28faf00a7b61deb20f612e8c4c69a7e
gumrahsindar <gsindar@gmail.com>
2024-10-26 23:13:18 +0300

    Refactor Header component and add styles

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Button from './Button.vue'
+import Logo from './Logo.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="layout">
+      <Logo />
+      <Button class="btn-header title-sm-bold" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 50px;
+
+  @media (max-width: 64rem) {
+    padding-top: 40px;
+  }
+
+  @media (max-width: 48rem) {
+    padding-top: 24px;
+  }
+}
+</style>
