50b525e796437d29e502b74c19c1eb40b4307533
gumrahsindar <gsindar@gmail.com>
2024-10-26 23:12:52 +0300

    Refactor layout and component structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Hero from './components/Hero.vue'
+import Testimonial from './components/Testimonial.vue'
+import BaseLayout from './layouts/BaseLayout.vue'
+</script>
 
 <template>
-  <div>Hello World</div>
+  <BaseLayout>
+    <template #header>
+      <Header />
+    </template>
+    <template #main>
+      <Hero />
+      <Testimonial />
+    </template>
+    <template #footer>
+      <Footer />
+    </template>
+  </BaseLayout>
 </template>
 
 <style scoped></style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot name="main"></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</template>
